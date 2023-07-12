06d55182b1eafc6eb430f570e94209375002d36f
nrblqsh <b032110395@student.utem.edu.my>
2023-07-12 12:02:20 +0800

    Lab 14 Exercise 5 Part 1

---
+++ TCPSummationAndMultiplication/src/controller/NumberCalculator.java
@@ -0,0 +1,34 @@
+package controller;
+
+
+/**
+ * This is a controller class that manage the arithmetic operations.
+ * 
+ * @author norbalqish
+ *
+ */
+
+public class NumberCalculator {
+	
+	
+	/**
+	 * A method that will get the summation from two numbers
+	 * @param number1
+	 * @param number2
+	 *  @param number3
+	 * @return
+	 */
+	public int getSum(int number1, int number2, int number3) {
+		
+		return number1 + number2 + number3;
+		
+	}
+	public int getMultiplication(int number1, int number2, int number3) {
+		
+		return number1 * number2 * number3;
+		
+	}
+
+	
+
+}
